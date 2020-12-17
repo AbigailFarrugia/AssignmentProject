862ec8a89551ec2f8e741315210aaef62575735a
LunarChild101 <abigail.farrugia.d56775@mcast.edu.mt>
2020-12-17 18:05:51 +0100

    Imported assests and created a plane script
    I imported the game background and the game character into unity. I setted them both up. Then I created a player script and added the needed pseudocode.

---
+++ Assignment/Assignment (hopping game)/Assets/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When spacebar is clicked the character will jump on the first platform and the game starts
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
