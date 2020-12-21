fb828ddb21bd8a9978a3228b9133d33f397b03f3
LunarChild101 <abigail.farrugia.d56775@mcast.edu.mt>
2020-12-21 12:48:54 +0100

    Added Jumping action to player
    When right arrow (d key as alternative) or left arrow (a key as alternative) is pressed player jumps upwards and lands back on testplatform.

--- Assignment/Assignment (hopping game)/Assets/PlayerScript.cs
+++ Assignment/Assignment (hopping game)/Assets/PlayerScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //The jump's force
+    [SerializeField]
+    private float _force = 1000f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,19 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //If right or left arrow is pressed player should jump
+        bool pressedJumpButton1 = Input.GetButtonDown("RightKey");
+        bool pressedJumpButton2 = Input.GetButtonDown("LeftKey");
+
+        //If rightkey is pressed pressedJumpButton = True
+        if(pressedJumpButton1 || pressedJumpButton2)
+        {
+            //storing the rigidbody2D component into the variable rigidbody
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            //setting rigidbody to zero Vector3(x,y,z)
+            rigidbody.velocity = Vector3.zero;
+            //adding jumping force to rigidbody Vector3(0,1,0) * force(1000f)
+            rigidbody.AddForce(Vector3.up * _force);
+        }
     }
 }
