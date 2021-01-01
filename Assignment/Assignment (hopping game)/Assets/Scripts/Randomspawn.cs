442d1c37aa40999bdd734d116790d51078ea501f
seanf13 <74544978+seanf13@users.noreply.github.com>
2021-01-01 17:17:50 +0100

    Random testting, most didnt work
    tests trying to make the platform spawn randomly

---
+++ Assignment/Assignment (hopping game)/Assets/Scripts/Randomspawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomspawn : MonoBehaviour {
+public GameObject Theplatform;
+public float placeX;
+public float placeY;
+
+    void Start()
+ {
+placeX = Random.Range(-9,-5);
+placeY = Random.Range(-17,105);
+Theplatform.transform.position = new Vector3(placeX,placeY,5);
+ }
+
+}
---
+++ Assignment/Assignment (hopping game)/Assets/Scripts/trial1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trial1 : MonoBehaviour
+
+{
+    public Transform prefab;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        } 
+    }
+}
