5fa8c6a51a40d17798b1f3f466b31f92ae841878
LunarChild101 <abigail.farrugia.d56775@mcast.edu.mt>
2021-01-04 23:52:57 +0100

    Added in Platform spawners & Generators
    Inserted platform spawners that spawn the platforms randomly and created a platform generation system which also spawns the platforms randomly.

---
+++ Assignment/Assignment (hopping game)/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+    public GameObject[] thePlatforms;
+    private float platformHeight;
+    private int platformSelector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformHeight = thePlatform.GetComponent<BoxCollider2D>().size.y;
+        int rand = Random.Range(0, thePlatforms.Length);
+        Instantiate(thePlatforms[rand], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < generationPoint.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + platformHeight + distanceBetween, transform.position.z);
+            platformSelector = Random.Range(0, thePlatforms.Length);
+            Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+        }
+    }
+}
