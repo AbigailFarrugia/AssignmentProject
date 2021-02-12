87ff0a1a43100928c1714345a654c632db4bfa6d
LunarChild101 <abigail.farrugia.d56775@mcast.edu.mt>
2021-02-12 09:03:37 +0100

    Fixed issues and remade accidentally deleted content

---
+++ Assignment/Assignment (hopping game)/Assets/Icon & Splash image/Wait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    public float wait_time = 5f;
+    void Start()
+    {
+        StartCoroutine(wait());
+    }
+    IEnumerator wait()
+    {
+        //Telling unity to wait for 5 seconds(animation time)
+        yield return new WaitForSeconds(wait_time);
+
+        //Telling unity to load next scene after the time runs out
+        SceneManager.LoadScene(+1);
+    }
+}
--- Assignment/Assignment (hopping game)/Assets/Scripts/ScoreKeeper.cs
+++ Assignment/Assignment (hopping game)/Assets/Scripts/ScoreKeeper.cs
@@ -6,25 +6,27 @@ using UnityEngine.UI;
 public class ScoreKeeper : MonoBehaviour
 {
     [SerializeField]
-    private int currentScore = 0;
+
+    //Adds Score field in Unity
+    public Text Score;
+
+    //Adds menuScore field in Unity
+    public Text menuScore;
+    private int _currentScore = 0;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //recalls last _currentScore stored
+        menuScore.text = PlayerPrefs.GetInt("MenuScore", 0).ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void IncrementScore()
     {
         //takeing the _currentscore + 1
+        Score.text = "Score: " + _currentScore.ToString();
+        _currentScore++;
 
-        Text ScoreText = GetComponent<Text>();
-        ScoreText.text = "Score: " + currentScore.ToString();
-        currentScore++;
+        //Storing _currentScore
+        PlayerPrefs.SetInt("MenuScore", _currentScore);
     }
 }
